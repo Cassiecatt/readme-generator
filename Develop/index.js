65237772babb67e0f6666c3e4f1cd2221e9134ca
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-07-25 12:57:00 -0700

    added writefile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,15 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+ //variable holding markdown file
+ const markDown = require('./utils/generateMarkdown');
 
 // array of questions for user
 const questions = () => {
     return inquirer.prompt ([
         {
             type: "input",
-            name: "name",
+            name: "title",
             message: "What is your project's name? (Required)",
             validate: (nameInput) => {
                 if (nameInput) {
@@ -56,17 +60,10 @@ const questions = () => {
             }
         },
         {
-            type: "input",
+            type: "checkbox",
             name: "license",
-            message: "Provide a license (Required)",
-            validate: (nameInput) => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log("Please provide a license");
-                    return false;
-                }
-            }
+            message: "What kind of license should your project have (Required)",
+            choices: ["NPM", "Github", "None"]
         },
         {
             type: "input",
@@ -96,16 +93,32 @@ const questions = () => {
         }
     ]);
 };
-questions().then(answers => console.log(answers));
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// need to update
+function writeFile(fileName, data) {
+    fs.writeFile('../README.md', data, (err) => {
+        if (err) {
+            console.log(err)
+            return;
+        } else {
+            console.log("README successful");
+        }
+    });
+};
 
 // function to initialize program
 function init() {
+    questions().then(answers => writeFile("README.md", markDown(answers))
+    );
 
 }
 
 // function call to initialize program
 init();
+
+
+
+
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  //[here](# ${data.description})
+  //add jump links for each section
+  // example * [Installation](#installation) ?
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  ${data.license}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
 `;
 }
 
+// console.log(generateMarkdown())
+
 module.exports = generateMarkdown;
+
+
+
