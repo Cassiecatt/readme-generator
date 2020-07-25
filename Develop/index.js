1b9ce5d5e4e2179ec37c2f76bb13ff30589267d1
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-07-25 10:49:16 -0700

    added question prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,101 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
 
-];
+// array of questions for user
+const questions = () => {
+    return inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your project's name? (Required)",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your project's name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a description of your project (Required)",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a description of your project!")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What are the steps required to install your project? (Required)",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Provide steps required to install project!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Provide instructions and examples for use (Required)",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please provide instructions and examples for use!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "Provide a license (Required)",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please provide a license");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Provide contribution guidelines for your application (Required)",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please provide contributoin guidelines!")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Provide tests for your application and examples on how to run them (Required)",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please provide tests for your application")
+                    return false;
+                }
+            }
+        }
+    ]);
+};
 
 // function to write README file
 function writeToFile(fileName, data) {
